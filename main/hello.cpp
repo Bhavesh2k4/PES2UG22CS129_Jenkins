6d0287cd192e6f071282967d8e09e63ea9048f4f
Bhavesh Budharaju <122691714+Bhavesh2k4@users.noreply.github.com>
2025-03-14 21:46:43 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "This is Bhavesh Budharaju!";
+  cout << "PES2UG22CS129 - LAB 08";
   return 0;
 }
